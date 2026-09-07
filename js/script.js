3e7ae4f0fe6507f0aebf4398b97b796ec35fb005
unknown <alupro5706@gmail.com>
2026-09-07 13:37:11 +0700

    feat: buat landing page awal UMKM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#promoButton");
 
-promoButton.addEventListener("#click", () => {
+promoButton.addEventListener("click", () => {
     promoButton.textContent = "Promo: Beli 2 gratis 2!";
     console.log("Prommo Kopi Nusa berhasil ditampilkan.");
 });
